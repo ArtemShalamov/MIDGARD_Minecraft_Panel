f94d2dca485b05636295d23c12098bbf52e476b0
TheEnderOfficial <70067741+TheEnderOfficial@users.noreply.github.com>
2021-03-02 19:25:34 +0300

    [FEATURE] ADD FRONTEND FOLDER INTO GITIGNORE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api.db.user import User
 test = User()
 print(test.login('YT_TheEnderOff', '99859985ender'))
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='frontend')
 
 if __name__ == "__main__":
 	app.run(port=80)
